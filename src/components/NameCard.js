d523cd992e045b43247237447cb5d23af632e949
SHERlocked93 <443033099@qq.com>
2020-09-12 16:36:02 +0800

    feat: react 组件写成函数的形式

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -1,22 +1,21 @@
 import React from 'react'
 
-class NameCard extends React.Component {
-    render() {
-        const { name, number, isHuman, tags } = this.props
-        return (
-          <div className='alert alert-success'>
-              <h4 className='alert-heading'>{ name }</h4>
-              <ul>
-                  <li>电话:{ number }</li>
-                  <li>{ isHuman ? '人类' : '外星生物' }</li>
-                  <hr/>
-                  <p>
-                      { tags.map((tag, idx) => <span className='badge badge-pill badge-primary' key={ idx }>{ tag }</span>) }
-                  </p>
-              </ul>
-          </div>
-        )
-    }
+const NameCard = props => {
+    const { name, number, isHuman, tags } = props
+    return (
+      <div className='alert alert-success'>
+          <h4 className='alert-heading'>{ name }</h4>
+          <ul>
+              <li>电话:{ number }</li>
+              <li>{ isHuman ? '人类' : '外星生物' }</li>
+              <hr/>
+              <p>
+                  { tags.map((tag, idx) => <span className='badge badge-pill badge-primary' key={ idx }>{ tag }</span>) }
+              </p>
+          </ul>
+      </div>
+    )
 }
+
 
 export default NameCard
