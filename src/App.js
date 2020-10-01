f360cb70a9a76a714f518bb51d43a75703d52769
SHERlocked93 <443033099@qq.com>
2020-10-01 16:22:42 +0800

    feat: router param

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './containers/Home'
+import Create from './containers/Create'
 
 function App() {
     return (
-      <div className="App">
-          <Home/>
-      </div>
+      <Router>
+          <div className="App">
+              <Route path='/' exact component={ Home }></Route>
+              <Route path='/create' component={ Create }></Route>
+              <Route path='/edit/:id' component={ Create }></Route>
+          </div>
+      </Router>
     )
 }
 
---
+++ src/containers/Create.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Create = ({ match }) => {
+    return (
+      <div>
+          create.js edit match is : { match.params.id }
+      </div>
+    )
+}
+
+export default Create
